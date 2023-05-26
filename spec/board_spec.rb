bb32436479cdc487041a8c145fc2838cd8f86f67
andi lovetto <andilovetto@gmail.com>
2023-05-26 15:19:38 -0600

    fix: Amend x on test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Board do
     expect(@board).to be_a(Board)
   end
 
-  xgit sit 'is initialized with cells' do
+  xit 'is initialized with cells' do
     cells = @board.cells
     expect(cells).to be_a(Hash)
     expect(cells.count).to eq(16)
