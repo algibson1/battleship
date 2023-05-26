faa08737a7025b87f15c482cdae91c2438031e2e
andi lovetto <andilovetto@gmail.com>
2023-05-26 15:15:59 -0600

    test: Test for valid and invalid ship placements

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,24 +3,49 @@ require 'spec_helper'
 RSpec.describe Board do
   before do 
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   it 'exists' do
     expect(@board).to be_a(Board)
   end
 
-  it 'is initialized with cells' do
+  xit 'is initialized with cells' do
     cells = @board.cells
     expect(cells).to be_a(Hash)
     expect(cells.count).to eq(16)
     expect(cells.values.first).to be_a(Cell)
   end
 
-  it 'has valid and unvalid cell coordinates' do
+  xit 'has valid and invalid cell coordinates' do
     expect(@board.valid_coordinate?("A1")).to eq(true)
     expect(@board.valid_coordinate?("D4")).to eq(true)
     expect(@board.valid_coordinate?("A5")).to eq(false)
     expect(@board.valid_coordinate?("E1")).to eq(false)
     expect(@board.valid_coordinate?("A22")).to eq(false)
+  end
+
+  xit 'returns invalid if ship length does not match coordinates given' do
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  xit 'returns invalid if coordinates not consecutive' do
+    expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq(false)
+    expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to eq(false)
+  end
+
+
+  xit 'can not place ships diagonally' do
+    expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to eq(false)
+  end
+
+  xit 'can determine if ship placement is valid' do
+    expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to eq(true)
+    expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
 end
