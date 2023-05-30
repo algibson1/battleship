65c2fe39d89b0b65a1df2d409b9e18b75420d428
Artemy Gibson <artemygibson@gmail.com>
2023-05-30 14:21:08 -0700

    feat: work on take_turn method and helpers

--- lib/battle.rb
+++ lib/battle.rb
@@ -19,9 +19,9 @@ class Battle
     if user_input == "p" 
       #placeholder: "customize" method. Ask player if they'd like to play a custom game or standard game. Custom choice: reassigns board size and ships. Standard choice: moves forward with the defaults we already made
       place_computer_ships
-      instructions
+      set_up
     elsif user_input == "q"
-
+      #edit later to account for alternative answers
     end
   end
 
@@ -40,10 +40,10 @@ class Battle
     end
   end
 
-  def instructions
+  def set_up
     puts "I have laid out my ships on the grid.\nYou now need to lay out your ships.\nThe Cruiser is three units long and the Submarine is two units long.\n#{@user_board.render(true)}"
     user_ship_placement
-    #placeholder for take_turn method
+    take_turn
   end
 
   def user_ship_placement
@@ -60,26 +60,79 @@ class Battle
     end
   end
 
+  def turn_setup
+    puts "==========COMPUTER BOARD=========="
+    puts @computer_board.render
+    puts "==========PLAYER BOARD=========="
+    puts @user_board.render(true)
+    puts "Enter the coordinate for your shot:"
+  end
+
+  def fire_player_shot(shot)
+    until @computer_board.valid_coordinate?(shot) && 
+      !@computer_board.cells[shot].fired_upon?
+      if !@computer_board.valid_coordinate?(shot)
+        puts "Please enter a valid coordinate"
+      else
+        #cannot choose a cell that has already been fired upon
+        puts "You already shot that square! Pick another:"
+      end
+      shot = gets.chomp
+    end
+    @computer_board.cells[shot].fire_upon
+  end
+
+  def generate_computer_shot
+    valid_cells = @user_board.cells.select do |coordinate, cell|
+      cell.fired_upon? == false
+    end
+    valid_cells.keys.sample
+  end
+
+  def calculate_results(user_shot, computer_shot)
+    user_render = @computer_board.cells[user_shot].render
+    computer_render = @user_board.cells[computer_shot].render
+    rendered_results = [user_render, computer_render]
+    results = []
+    rendered_results.each do |result|
+      if result == "X" || result == "H"
+        results << "hit"
+      else 
+        results << "miss"
+      end
+    end
+    results
+  end
   #psuedocode below
   # take turn method
-    #Loop starts here
-    # first: render and puts both computer and player boards
-        #player board will render with (true) in order to see the ships
-    # Second: Player is prompted to choose a coordinate to fire upon (a cell method)
-        #If invalid coordinate selected, will be prompted again
-        #cannot choose a cell that has already been fired upon
-              # Generate array of already used cells by finding all cells that have a true fired_upon status
-                  #Maybe put this outside of the loop, as a collector, which we add to with each turn, instead of generating new array each time through the loop
-              # User will get 'invalid coordinate, please choose again' prompt unless they choose a valid coordinate (check against @cells.keys)
-              # User may also get alternative message: "You already fired on that coordinate" (check against array of fired upon coordinates)
-        # will need to call cell.fire_upon, which changes how the cell will render, and will decrease ship health if a ship is present
-        # Re-calculate computer health score. Generate array of ship health, call array.sum 
-    # Third: Computer selects a random coordinate to fire upon
-        #Need: array of valid coordinates, then call #select to choose random cell
-              # Generate valid_cells array by finding all cells that are not fired upon yet
-        # call #fire_upon on the randomly selected cell
-        #recalculate user health score
-    # Fourth: Puts the results to terminal
+  def take_turn
+    until @user_health == 0 || @computer_health == 0
+      turn_setup
+      user_shot = gets.chomp
+      fire_player_shot(user_shot)
+      computer_shot = generate_computer_shot
+      @user_board.cells[computer_shot].fire_upon
+      #results
+      results = calculate_results(user_shot, computer_shot)
+      puts "Your shot on #{user_shot} was a #{results[0]}"
+      puts "My shot on #{computer_shot} was a #{results[1]}"
+      puts "---"
+      if @user_board.cells[computer_shot].ship.sunk?
+        puts "Get SUNK! I sank your #{@user_board.cells[computer_shot].ship.name}!"
+      end
+      if @computer_board.cells[user_shot].ship.sunk?
+        puts "Oh no, you sunk my #{@computer_board.cells[user_shot].ship.name}!"
+      end
+      if results[0] == "hit"
+        @computer_health -= 1
+      end
+      if results[1] == "hit"
+        @user_health -= 1
+      end
+
+      # Fourth: Puts the results to terminal
+      # Re-calculate computer health score. Generate array of ship health, call array.sum 
+      #recalculate user health score
         # "Your shot on #{computer's cell} was a #{result-- miss or hit}"
         # "My shot on #{player's cell} was a #{result -- miss or hit}"
         # If a ship was sunk, include "(You or I, as in user or computer) sunk (your or my) #{ship}!"
@@ -88,7 +141,8 @@ class Battle
             # If all ships are sunk, that would also be end of game. Could eliminate the computer health and user health attributes, tests, and recalculations
             #Coud also add a test into spec like 'can check status of ships', sink a ship, then run expects statements to see status of the ships
 
-
+    end
+  end
   #End Game method
     #If computer health is 0 (or if all computer ships sunk)
         #"You Won!"
